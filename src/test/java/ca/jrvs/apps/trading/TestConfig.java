91e958b7d32b51b6a1b3ad421439d691d825f632
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-07-17 14:24:13 -0400

    fixing test contexts

--- src/test/java/ca/jrvs/apps/trading/TestConfig.java
+++ src/test/java/ca/jrvs/apps/trading/TestConfig.java
@@ -2,6 +2,8 @@ package ca.jrvs.apps.trading;
 
 import javax.sql.DataSource;
 import org.apache.commons.dbcp2.BasicDataSource;
+import org.apache.http.conn.HttpClientConnectionManager;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -18,5 +20,13 @@ public class TestConfig {
     dataSource.setUsername("postgres");
     dataSource.setPassword("password");
     return dataSource;
+  }
+
+  @Bean
+  public HttpClientConnectionManager httpClientConnectionManager() {
+    PoolingHttpClientConnectionManager cm = new PoolingHttpClientConnectionManager();
+    cm.setMaxTotal(50);
+    cm.setDefaultMaxPerRoute(50);
+    return cm;
   }
 }
