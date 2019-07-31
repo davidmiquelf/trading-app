08212b17d044f8bd0600ec0d466dc47a3693a61f
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-07-31 15:01:00 -0400

    rds password

--- dll/psql_container.sh
+++ dll/psql_container.sh
@@ -14,7 +14,7 @@ docker build -t jrvs-psql .
 docker run --name jrvs-psql \
 --restart unless-stopped \
 -e "PSQL_URL=jdbc:postgresql://trading-app-david.cqjlilomcylr.us-east-1.rds.amazonaws.com:5432/jrvstrading" \
--e POSTGRES_PASSWORD=password \
+-e POSTGRES_PASSWORD=davidpassword \
 -e POSTGRES_DB=jrvstrading \
 -e POSTGRES_USER=postgres \
 --network trading-net \
