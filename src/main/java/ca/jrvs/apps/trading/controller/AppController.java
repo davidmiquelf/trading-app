645456724aeea1c71404a4a94679e73579f12dc2
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-08-01 11:57:06 -0400

    health fix

--- src/main/java/ca/jrvs/apps/trading/controller/AppController.java
+++ src/main/java/ca/jrvs/apps/trading/controller/AppController.java
@@ -2,7 +2,7 @@ package ca.jrvs.apps.trading.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/")
 public class AppController {
 
-  @PutMapping(path = "/health")
+  @GetMapping(path = "/health")
   @ResponseStatus(HttpStatus.OK)
   public void healthCheck() {
   }
