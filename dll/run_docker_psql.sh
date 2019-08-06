172a5ef9fefa5c69924c86c10ad5e1e1e6f4c1e0
Davidmiquelf <david.faig.jrvs.ca@gmail.com>
2019-08-06 09:25:30 -0400

    psql script remade

---
+++ dll/run_docker_psql.sh
@@ -0,0 +1,21 @@
+#!/bin/#!/usr/bin/env bash
+docker ps -a | awk '{ print $1,$2 }' | grep jrvs-psql \
+| awk '{print $1 }' | xargs -I {} docker container stop {}
+
+docker ps -a | awk '{ print $1,$2 }' | grep jrvs-psql \
+| awk '{print $1 }' | xargs -I {} docker container rm {}
+
+cd ../psql
+
+docker build -t jrvs-psql .
+
+docker run --name jrvs-psql \
+--restart unless-stopped \
+-e POSTGRES_PASSWORD=password \
+-e POSTGRES_DB=jrvstrading \
+-e POSTGRES_USER=postgres \
+--network trading-net \
+-d -p 5432:5432 jrvs-psql
+
+cd ../dll
+
