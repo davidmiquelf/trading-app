acf13ad4dabe1401b3798e1e72fbf6af4a7dc3fb
davidmiquelf <51926819+davidmiquelf@users.noreply.github.com>
2019-08-05 23:10:10 -0400

    nvm about scripts.

--- dll/run_local.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export IEX_PUB_TOKEN="pk_f0966987a4e34207821ed24dbfdf9bb2"
-export PSQL_USER=postgres
-export PSQL_PASSWORD=password
-export PSQL_URL="jdbc:postgresql://localhost:5432/jrvstrading"
-
-source ./jar.sh
-
-echo "done!"
